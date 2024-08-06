88b95603a1dd124f077f0c0dfed6a2c5cab2f8a2
Shubham Babar <babarshubham133@gmail.com>
2024-08-06 14:12:01 +0530

    Doubly Circular Linked List Generic

---
+++ Doubly_Circular_Gen.cpp
@@ -0,0 +1,379 @@
+// Generic Doubly Circular LinkedList
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+struct nodeDC
+{
+    T data;
+    struct nodeDC *next;
+    struct nodeDC *prev;
+};
+
+template <class T>
+class DoublyCL
+{
+    private:
+        struct nodeDC<T> * First;
+        struct nodeDC<T> * Last;
+        int iCount;
+
+    public:
+        DoublyCL();
+
+        void Display();
+        int Count();
+
+        void InsertFirst(T No);
+        void InsertLast(T No);
+        void InsertAtPos(T No, int iPos);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int iPos);
+};
+
+template <class T>
+DoublyCL<T>:: DoublyCL()
+{
+    First = NULL;
+    Last = NULL;
+    iCount = 0;
+}
+
+template <class T>
+void DoublyCL<T>::Display()
+{
+    if(First == NULL && Last == NULL)
+    {
+        cout<<"Linked List is emprty\n";
+        return;
+    }
+
+    cout<<"<=>";
+    do
+    {
+        cout<<"| "<<First->data <<" |<=>";
+        First = First -> next;
+    }while(Last->next != First);
+
+    cout<<"\n";
+}
+
+template <class T>
+int DoublyCL<T>::Count()
+{
+    return iCount;
+}
+
+template <class T>
+void DoublyCL<T>::InsertFirst(T No)
+{
+    struct nodeDC<T> * newn = NULL;
+
+    newn = new nodeDC<T>;
+
+    newn->data = No;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if((First == NULL) && (Last == NULL))
+    {
+        First = newn;
+        Last = newn;
+    }
+    else
+    {
+        newn->next = First;
+        First->prev = newn;
+        First = newn;
+    }
+    Last -> next = First;
+    First -> prev = Last;
+
+    iCount++;
+}
+
+template <class T>
+void DoublyCL<T>::InsertLast(T No)
+{
+    struct nodeDC<T> * newn = NULL;
+
+    newn = new nodeDC<T>;
+
+    newn->data = No;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if((First == NULL) && (Last == NULL))
+    {
+        First = newn;
+        Last = newn;
+    }
+    else
+    {
+        Last ->next = newn;
+        newn->prev = Last;
+        Last = newn;
+    }
+    Last -> next = First;
+    First -> prev = Last;
+
+    iCount++;    
+}
+
+template <class T>
+void DoublyCL<T>::InsertAtPos(T No, int iPos)
+{
+    struct nodeDC<T> * temp = NULL;
+    struct nodeDC<T> * newn = NULL;
+
+    int i = 0;
+
+    if(iPos < 1 || iPos > iCount+1)
+    {
+        cout<<"Invalid postion\n";
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        InsertFirst(No);
+    }
+    else if(iPos == iCount+1)
+    {
+        InsertLast(No);
+    }
+    else
+    {
+        newn = new nodeDC<T>;
+
+        newn->data = No;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        temp = First;
+
+        for(i = 1; i < iPos -1; i++)
+        {
+            temp = temp -> next;
+        }
+
+        newn->next = temp->next;
+        temp->next->prev = newn;
+
+        temp->next = newn;
+        newn->prev = temp;
+
+        iCount++;
+    }
+}
+
+template <class T>
+void DoublyCL<T>::DeleteFirst()
+{
+    if(First == NULL && Last == NULL) 
+    {
+        cout<<"LL is empty\n";
+        return;
+    }
+    else if(First == Last) 
+    {
+        delete First;
+        First = NULL;
+        Last = NULL;
+    }
+    else  
+    {
+        First = First -> next;
+        delete Last->next;
+        First -> prev = Last;
+        Last -> next = First;
+    }
+    iCount--;
+}
+
+template <class T>
+void DoublyCL<T>::DeleteLast()
+{
+    if(First == NULL && Last == NULL)
+    {
+        cout<<"LL is empty\n";
+        return;
+    }
+    else if(First == Last)  
+    {
+        delete First;
+        First = NULL;
+        Last = NULL;
+    }
+    else 
+    {
+        Last = Last -> prev;
+        delete First -> prev;
+
+        Last -> next = First;
+        First -> prev = Last;
+    }
+    iCount--;    
+}
+
+template <class T>
+void DoublyCL<T>::DeleteAtPos(int iPos)
+{
+    struct nodeDC<T> * temp = NULL;
+
+    int i = 0;
+
+    if(iPos < 1 || iPos > iCount)
+    {
+        cout<<"Invalid postion\n";
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        DeleteFirst();
+    }
+    else if(iPos == iCount)
+    {
+        DeleteLast();
+    }
+    else
+    {
+        temp = First;
+
+        for(i = 1; i < iPos -1; i++)
+        {
+            temp = temp -> next;
+        }
+
+        temp->next = temp->next->next;
+        delete temp->next->prev;
+        temp->next->prev = temp;
+
+        iCount--;
+    }    
+}
+
+int main()
+{
+    cout<<"-------------------------------------------------------------\n";
+    cout<<"\t\t Doubly Circular LinkedList \n";
+    cout<<"-------------------------------------------------------------\n";
+
+    // LL of integer
+    cout<<"-------------- LinkdList of Integers --------------\n";
+
+    DoublyCL<int> *iobj = new DoublyCL<int>();
+    int iRet = 0;
+
+    iobj->InsertFirst(51);
+    iobj->InsertFirst(21);
+    iobj->InsertFirst(11);
+
+    iobj->InsertLast(101);
+    iobj->InsertLast(111);
+    iobj->InsertLast(121);
+    
+    iobj->Display();
+    iRet = iobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    iobj->InsertAtPos(105,5);
+    iobj->Display();
+    iRet = iobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    iobj->DeleteAtPos(5);
+    iobj->Display();
+    iRet = iobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+
+    // LL of character
+    cout<<"-------------- LinkdList of Chracters --------------\n";
+
+    DoublyCL<char> *cobj = new DoublyCL<char>();
+
+    cobj->InsertFirst('D');
+    cobj->InsertFirst('F');
+    cobj->InsertFirst('R');
+
+    cobj->InsertLast('E');
+    cobj->InsertLast('Y');
+    cobj->InsertLast('U');
+    
+    cobj->Display();
+    iRet = cobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    cobj->InsertAtPos('W',5);
+    cobj->Display();
+    iRet = cobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    cobj->DeleteAtPos(5);
+    cobj->Display();
+    iRet = cobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+
+    // LL of float
+    cout<<"-------------- LinkdList of Floats --------------\n";
+
+    DoublyCL<float> *fobj = new DoublyCL<float>();
+
+    fobj->InsertFirst(90.78f);
+    fobj->InsertFirst(78.99f);
+    fobj->InsertFirst(67.99f);
+
+    fobj->InsertLast(45.67f);
+    fobj->InsertLast(54.78f);
+    fobj->InsertLast(77.89f);
+    
+    fobj->Display();
+    iRet = fobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    fobj->InsertAtPos(88.56,5);
+    fobj->Display();
+    iRet = fobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    fobj->DeleteAtPos(5);
+    fobj->Display();
+    iRet = fobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+
+    // LL of double
+    cout<<"-------------- LinkdList of Doubles --------------\n";
+
+    DoublyCL<double> *dobj = new DoublyCL<double>();
+
+    dobj->InsertFirst(90.78978);
+    dobj->InsertFirst(78.99645);
+    dobj->InsertFirst(67.9934);
+
+    dobj->InsertLast(45.67867);
+    dobj->InsertLast(54.78534);
+    dobj->InsertLast(77.89324);
+    
+    dobj->Display();
+    iRet = dobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    dobj->InsertAtPos(88.56987,5);
+    dobj->Display();
+    iRet = dobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    dobj->DeleteAtPos(5);
+    dobj->Display();
+    iRet = dobj->Count();
+    cout<<"Number of elemensts are : "<<iRet<<endl;
+
+    return 0;
+}
